0c60825545150187fe12971111b01bcc537ddc5d
crispinon <crispinon@gmail.com>
2022-01-20 16:57:14 +0100

    Update freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
+
+## curl https://raw.githubusercontent.com/crispinon/bootstrap/main/freebsd.sh | /bin/sh
+
 set -e
 
 # Util functions
 
 show_help() {
   cat << EOT
-usage: freebsd-bootstrap.sh [--gnome][--xfce][--vbox][--vmware]|[--help]
---gnome
-  Install Gnome 3
---xfce
-  Install XFCE
---vbox
-  Include VirtualBox additions and config
---vmware
-  Include VMWare additions and config
+usage: freebsd-bootstrap.sh [--user_nico][--ansible][--help]
+--user_nico
+  Create user Nico
+--ansible
+  Install Ansible
 --help
   This help
 EOT
@@ -27,10 +26,8 @@ write_to_file () {
 # Globals
 
 VERSION="0.1"
-GNOME=false
-XFCE=false
-VBOX=false
-VMWARE=false
+USER_NICO=false
+ANSIBLE=false
 
 while :; do
   case $1 in
@@ -38,21 +35,13 @@ while :; do
         show_help
         exit
         ;;
-    --gnome)
-        echo "Installing Gnome 3"
-        GNOME=true
+    --user_nico)
+        echo "Creating user Nico"
+        USER_NICO=true
         ;;
-    --xfce)
-        echo "Installing XFCE"
-        XFCE=true
-        ;;
-    --vbox)
-        echo "Using VirtualBox configuration"
-        VBOX=true
-        ;;
-    --vmware)
-        echo "Using VMWare configuration"
-        VMWARE=true
+    --ANSIBLE)
+        echo "Installing Ansible"
+        ANSIBLE=true
         ;;
     --) # End of all options.
         shift
