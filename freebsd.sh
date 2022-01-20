88eff4c46d27edd83b28ca8decfdcadd11592732
crispinon <crispinon@gmail.com>
2022-01-20 20:48:49 +0100

    Update freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -88,7 +88,8 @@ read -p "Do you want to check for create user 'Nico'? [y/N]: " answer
 case $answer in
   [Yy]*)
     if [ "$USER_NICO" = true ] && [ "$(id -u nico)" != '501' ]; then
-      pw useradd -n nico -u 501 -G wheel -s /bin/sh -c Nico -w random 
+      pw useradd -n nico -u 501 -G wheel -s /bin/sh -c Nico -w random
+      passwd nico
     fi
     ;;
 esac
